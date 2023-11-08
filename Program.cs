28c7b2b0a9933032cfde22ce60eabce0529c21a9
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-08 19:26:41 +0300

    commit6

--- Program.cs
+++ Program.cs
@@ -19,6 +19,17 @@
             urun2.Adi = "Karpuz";
             urun2.Fiyati = 80;
             urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+            }
         }
     }
 }
