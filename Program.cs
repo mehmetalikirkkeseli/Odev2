e67db7ed6e824fd28216c7d10249924d972fedf0
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-08 19:15:49 +0300

    commit5

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@
             urun1.Adi = "Elma";
             urun1.Fiyati = 15;
             urun1.Aciklama = "Amasya elamsı";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
         }
     }
 }
