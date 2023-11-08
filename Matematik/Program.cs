3600d962c6ab5d6b1cf9623d3faf83c64d23e070
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-08 22:28:21 +0300

    commit2

--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            DortIslem dortıslem = new DortIslem();
+            dortıslem.Topla(5, 6);
+
+            dortıslem.Topla(6, 9);
         }
     }
 }
