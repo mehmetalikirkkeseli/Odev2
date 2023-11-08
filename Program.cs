eba5014cc4be60edaf7cdcca7824fafbb462a04c
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-08 19:11:42 +0300

    commit4

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@
             string urunAdi = "Elma";
             double fiyati = 15;
             string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elamsı";
         }
     }
 }
--- Urun.cs
+++ Urun.cs
@@ -10,7 +10,7 @@ namespace Metotlar
     {
         public int Id { get; set; }
         public string Adi { get; set; }
-        public double Fİyati { get; set; }
+        public double Fiyati { get; set; }
         public string Aciklama { get; set; }
     }
 }
