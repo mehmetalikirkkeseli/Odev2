b0cbfdb8eb6bfe5ded886a12b5b17b6d44f27832
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-09 19:23:20 +0300

    commit3

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.Id = 1;
+            musteri1.TcNo = "2222222222";
+            musteri1.MusteriNo = "12345";
+            musteri1.SirketAdi = "?";
         }
     }
 }
