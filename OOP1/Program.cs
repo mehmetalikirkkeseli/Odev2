824a832c9f95971cd3ce509c0dc8d777aaa45602
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-09 15:05:10 +0300

    commit11

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -18,11 +18,8 @@
             ProductManager productManager = new ProductManager();
             productManager.Add(product1);
             Console.WriteLine(product1.ProductName);
+            
 
-            productManager.Topla2(3, 6);
-
-            int toplamaSonucu = productManager.Topla(3, 6);
-            Console.WriteLine(toplamaSonucu * 2);
 
             //int,double,bool... -> değer tip
             //diziler,class,abstract class,interface... -> referans tip
