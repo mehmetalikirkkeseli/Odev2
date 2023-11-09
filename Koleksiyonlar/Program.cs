b886f3c3cb8bdc1e29dd16fb459afcf735a9d518
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-09 15:54:51 +0300

    commit6

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -9,9 +9,11 @@
             Console.WriteLine(isimler[1]);
             Console.WriteLine(isimler[2]);
             Console.WriteLine(isimler[3]);
+
             isimler = new string[5];
             isimler[4] = "İlker";
             Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
 
         }
     }
