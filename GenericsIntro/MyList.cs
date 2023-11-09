f4927644ca941c99967a93f73e61d6a366c5be77
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-09 18:42:17 +0300

    commit6

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -19,6 +19,17 @@ namespace GenericsIntro
         {
             T[] tempArray = items;
             items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
         }
     }
 }
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -7,8 +7,12 @@
             MyList<string> isimler = new MyList<string>();
             isimler.Add("Engin");
 
-            List<string> liste = new List<string>();
-            Console.WriteLine(liste.Count);
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
 
         }
     }
