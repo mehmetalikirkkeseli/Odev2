3a99d203a2c83b1dfe71ebeca82e64d6e2d3a416
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-09 15:48:03 +0300

    commit2

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace Koleksiyonlar
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
+            string[] isimler = new string[] {"Engin","Murat","Kerem","Halil" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
         }
     }
 }
