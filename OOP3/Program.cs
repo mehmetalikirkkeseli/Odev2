7e364e1c925e3d58a74a8a7e601222692647b952
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-11-10 11:41:08 +0300

    commit9

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -5,13 +5,12 @@
         static void Main(string[] args)
         {
             IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
-            ihtiyacKrediManager.Hesapla();
-
             IKrediManager tasitKrediManager = new TasitKrediManager();
-            tasitKrediManager.Hesapla();
-
             IKrediManager konutKrediManager = new KonutKrediManager();
-            konutKrediManager.Hesapla();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager);
+
         }
     }
 }
